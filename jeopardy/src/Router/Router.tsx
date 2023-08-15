ff2a79fe9e90a5ec113d448a98fd282f182ec1d1
Kunfi <gianluca.kunfermann@gmail.com>
2023-08-15 15:41:16 +0200

    create simple stuff

--- jeopardy/src/Router/Router.tsx
+++ jeopardy/src/Router/Router.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import React from 'react'
 import MainPage from "../components/pages/MainPage";
 
 const Router = () => {
--- jeopardy/src/components/pages/MainPage.tsx
+++ jeopardy/src/components/pages/MainPage.tsx
@@ -1,8 +1,12 @@
+import { Box } from '@mui/material'
+import MainCard from "../organisms/MainCard"
 import React from 'react'
 
 const MainPage = () => {
   return (
-    <div>MainPage</div>
+    <Box>
+        <MainCard />
+    </Box>
   )
 }
 
