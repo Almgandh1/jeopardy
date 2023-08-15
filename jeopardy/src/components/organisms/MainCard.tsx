5f638524d4607fc847e1a3a2c64bd3d3f96e4060
Kunfi <gianluca.kunfermann@gmail.com>
2023-08-15 15:46:13 +0200

    some stuff

--- jeopardy/src/components/organisms/MainCard.tsx
+++ jeopardy/src/components/organisms/MainCard.tsx
@@ -1,9 +1,48 @@
+import Grid from '@mui/material/Grid'
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
 import React from 'react'
 
 const MainCard = () => {
+
+    const Item = styled(Paper)(() => ({
+        backgroundColor: '#98d6a9',
+        padding: 8,
+        textAlign: 'center',
+        color: 'black',
+      }));
+
   return (
-    <div>MainCard</div>
-  )
+    <>
+    <Grid container spacing={4}>
+
+      <Grid item xs={6}>
+        <Item elevation={3}>One</Item>
+      </Grid>
+
+      <Grid item xs={6}>
+        <Item elevation={3}>Two</Item>
+      </Grid>
+
+      <Grid item xs={6}>
+        <Item elevation={3}>Three</Item>
+      </Grid>
+
+      <Grid item xs={6}>
+        <Item elevation={3}>Four</Item>
+      </Grid>
+
+      <Grid item xs={6}>
+        <Item elevation={3}>Five</Item>
+      </Grid>
+
+      <Grid item xs={6}>
+        <Item elevation={3}>Six</Item>
+      </Grid>
+
+    </Grid>
+  </>
+)
 }
 
 export default MainCard
