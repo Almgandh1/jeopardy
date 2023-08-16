626bf2fa12e87430b3b809371ad1c84f584df7be
Kunfi <gianluca.kunfermann@gmail.com>
2023-08-16 15:44:58 +0200

    add buttons for point management

--- jeopardy/src/components/organisms/UserCard.tsx
+++ jeopardy/src/components/organisms/UserCard.tsx
@@ -1,17 +1,24 @@
-import Grid from '@mui/material/Grid'
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
 import React from 'react'
-import { Card } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import { Button } from '@mui/material';
+
+type UserCardProps = {
+  title: string;
+  score: number;
+  setScore: React.Dispatch<React.SetStateAction<number>>
+}
 
 
-
-const UserCard = ({ title, score }: { title: string; score: number }) => {
+const UserCard = ({ title, score, setScore }: UserCardProps) => {
     return (
+      <>
       <Paper>
         <h1>{title}</h1>
         <p>Score: {score}</p>
       </Paper>
+      <Button variant='contained' onClick={() => {setScore(score + 200)}}>Add Points</Button>
+      <Button variant='contained' onClick={() => {setScore(score - 200)}}>Remove Points</Button>
+      </>
     );
   };
 
--- jeopardy/src/components/pages/MainPage.tsx
+++ jeopardy/src/components/pages/MainPage.tsx
@@ -1,21 +1,25 @@
-import { Box, Card, Grid } from '@mui/material'
+import { Box,} from '@mui/material'
 import MainCard from "../organisms/MainCard"
-import React from 'react'
+import React, { useState } from 'react'
 import UserCard from '../organisms/UserCard'
 
 const MainPage = () => {
+  const [kunfiScore, setKunfiScore] = useState(0)
+  const [mircoScore, setMircoScore] = useState(0)
+  const [alecScore, setAlecScore] = useState(0)
+
   return (
       <Box>
         <MainCard />
         <Box style={{ display: 'flex', flexDirection: 'row' }}>
           <div style={{ marginRight: '10px' }}>
-            <UserCard title="John Doe" score={100} />
+            <UserCard title="Alec" score={alecScore} setScore={setAlecScore}/>
           </div>
           <div style={{ marginLeft: '10px', marginRight: '10px' }}>
-            <UserCard title="Dohn Joe" score={100} />
+            <UserCard title="Kunfi" score={kunfiScore} setScore={setKunfiScore}/>
           </div>
           <div style={{ marginLeft: '10px' }}>
-            <UserCard title="Jllaosdjdfj" score={100} />
+            <UserCard title="Mirco" score={mircoScore} setScore={setMircoScore}/>
           </div>
         </Box>
 
