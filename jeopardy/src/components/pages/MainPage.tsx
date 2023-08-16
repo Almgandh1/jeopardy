b256d4d376b289bccb4dcfdc2d27d3343d4d78f6
Geistlich Yves <yves.geistlich@windowslive.com>
2023-08-16 13:54:47 +0200

    quick update

--- jeopardy/src/components/pages/MainPage.tsx
+++ jeopardy/src/components/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Card } from '@mui/material'
+import { Box, Card, Grid } from '@mui/material'
 import MainCard from "../organisms/MainCard"
 import React from 'react'
 import UserCard from '../organisms/UserCard'
@@ -6,10 +6,14 @@ import UserCard from '../organisms/UserCard'
 const MainPage = () => {
   return (
       <Box>
-            <MainCard />
-            <UserCard title="John Doe" score={100}/>
-            <UserCard title="Yves" score={100}/>
-            <UserCard title="Jllaosdjdfj" score={100}/>
+        <MainCard />
+          <Box style={{ display: 'flex', flexDirection: 'row' }}>
+              
+            <UserCard title="John Doe" score={100} />
+            <UserCard title="Dohn Joe" score={100} />
+            <UserCard title="Jllaosdjdfj" score={100} />
+          </Box>
+
       </Box>
   )
 }
