41629c6190fb63498863313b001ced091214b251
Kunfi <gianluca.kunfermann@gmail.com>
2024-07-28 14:11:51 +0200

    size

--- jeopardy/src/components/organisms/QuestionDialog.tsx
+++ jeopardy/src/components/organisms/QuestionDialog.tsx
@@ -17,15 +17,15 @@ const QuestionDialog = ({handleClose, open, question, image, audio} : QuestionDi
         "& .MuiDialog-container": {
           "& .MuiPaper-root": {
             width: "100%",
-            maxWidth: "2000px",
+            maxWidth: "1500px",
             height: "100%",
-            maxHeight: "1000px"
+            maxHeight: "800px"
           },
         },
       }}>
         <DialogContent sx={{display: "flex", justifyContent: "center", flexDirection: "column"}}>
           <Typography variant='h3' sx={{justifyItems: "center", alignSelf: "center"}}>{question}</Typography>
-          {image && <img src={require("../data/images" + image)} alt='banana' style={{marginTop: "20px", width: "1000px", height:"750px", objectFit: "contain", flex: 1, alignSelf: "center"}}></img>}
+          {image && <img src={require("../data/images" + image)} alt='banana' style={{marginTop: "20px", width: "1000px", height:"700px", objectFit: "contain", flex: 1, alignSelf: "center"}}></img>}
           {audio && <audio controls style={{marginTop: "20px", alignSelf: "center"}}><source src={require("../data/audio" + audio)} type="audio/mpeg"></source></audio>}
         </DialogContent>
     </Dialog>
