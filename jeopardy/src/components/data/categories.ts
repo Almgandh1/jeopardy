811fcbd1414e1874bc54e20b79676f426e283a23
KBYves <yves.geistlich@windowslive.com>
2024-07-28 12:10:29 +0200

    lommisquiz

--- jeopardy/src/components/data/categories.ts
+++ jeopardy/src/components/data/categories.ts
@@ -1,18 +1,18 @@
 export const categories = {
     firstCategories: [
-        "r1-first",
-        "r1-second",
-        "r1-third",
-        "r1-fourth",
-        "r1-fifth",
-        "r1-sixth"
+        "AfriKA",
+        "Schätzen",
+        "Random",
+        "Filmmusik",
+        "Brand",
+        "Lolmypia"
     ],
     secondCategories: [
-        "r2-first",
-        "r2-second",
-        "r2-third",
-        "r2-fourth",
-        "r2-fifth",
-        "r2-sixth"
+        "Kantönligeist",
+        "Schätzen",
+        "Random",
+        "Guess The Song",
+        "Boozen",
+        "Fuessblal"
     ]
 } 
